631fa23e7cc49f31031da1586908cc842f80b205
davidcastro3 <davidcastrinho3@icloud.com>
2024-10-19 13:00:41 +0100

    Add files via upload

---
+++ altura_e_comprimento.py
@@ -0,0 +1,5 @@
+altura = float(input("a altura é:"))
+comprimento = float(input("o comprimento é:"))
+m2 = (altura*comprimento)/2
+print("a area é:",altura*comprimento)
+print("são necessarios  estes litros:",m2)
